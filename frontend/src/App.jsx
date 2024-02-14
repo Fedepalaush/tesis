ae52972a64655a8ead643b2b2fb5e2e1774f9cb6
unknown <federicopalacioush@hotmail.comgit config --global user.name Fedepalaushgit commit -m primera>
2024-02-13 22:03:10 -0300

    Cambios escritorio

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,52 +1,68 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import axios from "axios";
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavbarComp from "../components/NavbarComp";
+import SidebarComp from "../components/SidebarComp";
 
 function App() {
+  const [activosData, setActivosData] = useState([]);
 
-const [activosData, setActivosData] = useState([])
+  const endpoint = `${import.meta.env.VITE_API_URL}/activos/`;
 
-const endpoint = `${import.meta.env.VITE_API_URL}/activos/`
+  const fetchData = async () => {
+    console.log("fetching...");
+    const response = await axios.get(endpoint);
+    console.log(response);
+    const { data } = response;
+    setActivosData(data);
+    console.log(data);
+    return data;
+  };
 
-const fetchData = async()=> {
-  console.log('fetching...')
-  const response = await axios.get(endpoint)
-  console.log(response)
-  const {data} = response
-  setActivosData (data)
-  console.log(data)
-  return data
-}
+  const postData = async () => {
+    const ticker = "MSFT";
+    const precio = 320.45;
+    const body = { ticker, precio };
 
-const postData = async() => {
-  const ticker = 'MSFT'
-  const precio = 320.45
-  const body = {ticker, precio}
+    const response = await axios.post(endpoint, body);
+    console.log(response);
+    return response.data;
+  };
 
-  const response = await axios.post(endpoint, body)
-  console.log(response)
-  return response.data
-}
-
-const handleSendData = async() =>{
-  const newData = await postData()
-  if (newData){
-    setActivosData(prevState => [...prevState, newData])
-  }
-
-}
+  const handleSendData = async () => {
+    const newData = await postData();
+    if (newData) {
+      setActivosData((prevState) => [...prevState, newData]);
+    }
+  };
 
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   return (
-    <>
-    <ul>
-      {activosData.map(el => <li key={el.id}>{el.ticker}</li>)}
-    </ul>
-      <button onClick={handleSendData}>Crear Data</button>
-    </>
-  )
+    <Router>
+      <>
+        <NavbarComp />
+
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <SidebarComp/>
+          <div>
+            <ul>
+              {activosData.map((el) => (
+                <li key={el.id}>{el.ticker}</li>
+              ))}
+            </ul>
+            <button onClick={handleSendData}>Crear Data</button>
+            <Button variant="outline-dark">Presionamee</Button>
+          </div>
+        </div>
+      </>
+    </Router>
+  );
 }
 
-export default App
+export default App;
