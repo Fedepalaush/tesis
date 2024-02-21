420f75b6c0d2ee18338c6815ad4383f16050d6cc
Fedepalaush <federicopalacioush@gmail.com>
2024-02-21 20:33:46 -0300

    schema

---
+++ backend/core/schema.py
@@ -0,0 +1,22 @@
+import graphene
+from activos.models import Activo
+from graphene_django import DjangoObjectType
+
+class ActivosType(DjangoObjectType):
+    class Meta:
+        model = Activo
+        fields = ('id', 'ticker' , 'precio')
+
+
+class Query(graphene.ObjectType):
+    hello = graphene.String(default_value="Hi!")
+    activos = graphene.List(ActivosType)
+    activo = graphene.Field(ActivosType, id=graphene.ID())
+
+    def resolve_activos(self, info):
+        return Activo.objects.all()
+    
+    def resolve_activo(self,info,id):
+        return Activo.objects.get(id=id)
+
+schema = graphene.Schema(query=Query)   
